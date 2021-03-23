ea26216c62eba07b7af42d558fc6fb9df6137602
Salvador Gonzalez <salvador.gonzalez@wizeline.com>
2021-03-23 01:02:33 -0600

    Created the test to test data structure from getVideos.js

--- src/tests/helpers/getVideos.test.js
+++ src/tests/helpers/getVideos.test.js
@@ -11,5 +11,39 @@ describe('Tests of getVideos.js', () => {
     test('should return at least 1 element', () => {
         expect(videos.length).toBeGreaterThan(0);
     });
+
+    test('should return the correct data structure', () => {
+        const ytData = {
+            "items": [
+                {
+                    "id": {
+                        "kind": "youtube#video",
+                        "videoId": "123asd"
+                    },
+                    "snippet": {
+                        "channelId": "asd123",
+                        "title": "Test",
+                        "description": "This is a test",
+                        "thumbnails": {
+                            "medium": {
+                                "url": "https://localhost/test.jpg"
+                            }
+                        },
+                        "channelTitle": "Testing"
+                    }
+                }
+            ]
+        };
+        const videos = getVideos(ytData);
+        const video = [{
+            id: '123asd',
+            title: 'Test',
+            description: 'This is a test',
+            channel: { id: 'asd123', title: 'Testing' },
+            thumbnailUrl: 'https://localhost/test.jpg'
+        }];
+
+        expect(videos).toEqual(video);
+    });
     
 });
